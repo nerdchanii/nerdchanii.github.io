81833efdcb59c6fbb9f1a4000a2c8de9c3ed8e72
nerdchanii <nerdchanii@gmail.com>
2022-07-06 18:31:23 +0900

    action test

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Headers>
+        {/* <Headers>
           <Title />
           <Nav />
-        </Headers>
+        </Headers> */}
         <MainRoutes />
       </div>
     </BrowserRouter>
